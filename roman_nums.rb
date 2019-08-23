79907d9ab65920f1b337fe4375906392867fd23a
Ben <ben@benzeng.co.uk>
2019-08-23 10:18:18 +0100

    now converts 5

--- roman_nums.rb
+++ roman_nums.rb
@@ -6,24 +6,12 @@
 
 
 def convert_roman_num(number)
-  number == 4 ? "IV" : "I"*number
+  if number == 5
+    'V'
+  elsif number == 4
+    'IV'
+  else
+    'I' * number
+  end
 end
 
-=begin
-puts "enter number to convert to roman num:"
-input = gets.chomp
-
-numerals = [
-  { 1 => "I",
-    2 => "II",
-    3 => "III",
-    4 => "IV",
-    5 => "V",
-    6 => "VI",
-    7 => "VII",
-    8 => "VII",
-    9 => "IX",
-    10 => "X" }]
-
-  puts numerals
-=end
--- spec/roman_nums_spec.rb
+++ spec/roman_nums_spec.rb
@@ -1,16 +1,19 @@
-require_relative "../roman_nums.rb"
+require_relative '../roman_nums.rb'
 
-describe "roman numeral converter" do
+describe 'roman numeral converter' do
   it "returns 'I' when given 1" do
-    expect(convert_roman_num(1)).to eq "I"
+    expect(convert_roman_num(1)).to eq 'I'
   end
   it "returns 'II' when given 2" do
-    expect(convert_roman_num(2)).to eq "II"
+    expect(convert_roman_num(2)).to eq 'II'
   end
   it "returns 'III' when given 3" do
-    expect(convert_roman_num(3)).to eq "III"
+    expect(convert_roman_num(3)).to eq 'III'
   end
   it "returns 'IV' when given 4" do
-    expect(convert_roman_num(4)).to eq "IV"
+    expect(convert_roman_num(4)).to eq 'IV'
+  end
+  it "returns 'V' when given 5" do
+    expect(convert_roman_num(5)).to eq 'V'
   end
 end
